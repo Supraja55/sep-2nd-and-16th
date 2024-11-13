264657d2a538eec4a1c51b4440d2914f7f342038
Prasanth Kumar Nagothi <nprasanth4046@gmail.com>
2024-11-13 14:17:05 +0530

    Loops

--- javascript/script.js
+++ javascript/script.js
@@ -10,8 +10,7 @@ const studentId = 7643746;
 let studentName = "Hemanth";
 var studentEmail = "hemanth@google.com";
 let studentMobile = null;
-let isVerified = false
-
+let isVerified = false;
 
 // studentId = "736437" //Assignment is not allwoed for const varibales
 // studentName = "Manoj"
@@ -19,7 +18,6 @@ let isVerified = false
 // studentMobile = 123
 
 // console.table([studentId, studentEmail, studentName, studentMobile])
-
 
 /*
 Datatypes
@@ -31,11 +29,10 @@ Datatypes
 5. Undefined
 */
 
-let langName = "JavaScript"
+let langName = "JavaScript";
 
 // String Interpolation
 // console.log(`I like ${langName} language`);
-
 
 // Arithematic operators --> +,-,*,/,%,**
 
@@ -45,8 +42,66 @@ let langName = "JavaScript"
 
 // Assignment Operator --> =, +=, -=, *=, /= ...
 
-let a = 10
-let b = 20
-a += b // a = a + b
-console.log(a);
+// let a = 10
+// let b = 20
+// a += b // a = a + b
+// console.log(a);
 
+// Conditional Statements --> if, if else, if else if
+// let number = 0;
+// if (number > 0) {
+//     console.log("Positive Number");
+// } else if(number < 0) {
+//     console.log("Negative Number");
+// } else {
+//     console.log("The number is zero");
+// }
+
+// let number = 54
+// if (number % 2 === 0) {
+//     console.log(`${number} is a even number`);
+// } else {
+//     console.log(`${number} is a odd number`);
+// }
+
+// let num1 = 10
+// let num2 = 20
+// if (num1 > num2) {
+//     console.log(`${num1} is bigger than ${num2}`);
+// } else {
+//     console.log(`${num2} is bigger than ${num1}`);
+// }
+
+// find maximum between three numbers.
+
+// Increment Operator
+// let a = 10
+// Preincrement ++a, Post Increment a++
+// let b = ++a;
+// console.log(a, b);
+// Increment Operator
+// let c = 10
+// Predecrement --a, Post decrement a--
+// let d = --c;
+// console.log(c, d);
+
+// Loops --> for, while, do while
+// Factorial of natural numbers up to given number
+// let number = 5
+// let sum = 1
+// for(let i=1; i <= number; i++) {
+//     sum = sum * i
+// }
+// console.log(sum);
+
+
+// While loop
+let i = 0;
+let evenCount = 0
+while (evenCount <= 10) {
+    if (i%2 == 0) {
+        console.log(i);
+        evenCount++;
+    }
+    i++;
+}
